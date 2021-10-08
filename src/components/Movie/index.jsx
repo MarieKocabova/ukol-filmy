6f1ad80e34f51f2cc2be1edb80c6f422b661b6e4
MarieKocabova <maruspondeli@gmail.com>
2021-10-08 23:06:50 +0200

    css ready

--- src/components/Movie/index.jsx
+++ src/components/Movie/index.jsx
@@ -5,72 +5,273 @@ const Movie = () => {
   return (
     <div className="movie-section">
       <div className="movie-box">
-        <div className="rating">8,9%</div>
+        <div className="rating">
+          <b>8,9</b>
+          <small>/10</small>
+        </div>
         <img src="../assets/pulp-fiction.jpg" alt="pulp-fiction poster" className="poster" />
         <div className="info">
           <h2 className="title">Pulp Fiction</h2>
           <div className="meta">
             <p className="year">
-              <b>Rok vydání:</b> 1994
+              <b>Rok vydání:</b> <span>1994</span>
             </p>
             <p className="genre">
-              <b>Žárn:</b> crime, drama
+              <b>Žárn:</b> <span>crime, drama</span>
             </p>
             <p className="director">
-              <b>Režie:</b> Quentin Tarantino
+              <b>Režie:</b> <span>Quentin Tarantino</span>
             </p>
           </div>
-          <h3 className="cast-title">V hlavních rolích</h3>
+          <h3 className="cast-title">V hlavních rolích:</h3>
+          <div className="cast-innersection">
+            <div className="cast">
+              <div className="cast-name">
+                <b>John Travolta</b>
+              </div>
+              <div className="cast-as">
+                as <b>Vincent Vega</b>
+              </div>
+            </div>
+
+            <div className="cast">
+              <div className="cast-name">
+                <b>John Travolta</b>
+              </div>
+              <div className="cast-as">as Vincent Vega</div>
+            </div>
+
+            <div className="cast">
+              <div className="cast-name">
+                <b>John Travolta</b>
+              </div>
+              <div className="cast-as">as Vincent Vega</div>
+            </div>
+
+            <div className="cast">
+              <div className="cast-name">
+                <b>John Travolta</b>
+              </div>
+              <div className="cast-as">as Vincent Vega</div>
+            </div>
+          </div>
         </div>
-        <div className="cast-innersection"></div>
-        <p className="cast-name">John Travolta</p>
-        <p className="cast-as">as Vincent Vega</p>
       </div>
 
       <div className="movie-box">
-        <div className="rating">8,9%</div>
+        <div className="rating">
+          <b>8,9</b>
+          <small>/10</small>
+        </div>
         <img src="../assets/pulp-fiction.jpg" alt="pulp-fiction poster" className="poster" />
         <div className="info">
           <h2 className="title">Pulp Fiction</h2>
           <div className="meta">
             <p className="year">
-              <b>Rok vydání:</b> 1994
+              <b>Rok vydání:</b> <span>1994</span>
             </p>
             <p className="genre">
-              <b>Žárn:</b> crime, drama
+              <b>Žárn:</b> <span>crime, drama</span>
             </p>
             <p className="director">
-              <b>Režie:</b> Quentin Tarantino
+              <b>Režie:</b> <span>Quentin Tarantino</span>
             </p>
           </div>
-          <h3 className="cast-title">V hlavních rolích</h3>
+          <h3 className="cast-title">V hlavních rolích:</h3>
+          <div className="cast-innersection">
+            <div className="cast">
+              <div className="cast-name">
+                <b>John Travolta</b>
+              </div>
+              <div className="cast-as">
+                as <b>Vincent Vega</b>
+              </div>
+            </div>
+
+            <div className="cast">
+              <div className="cast-name">
+                <b>John Travolta</b>
+              </div>
+              <div className="cast-as">as Vincent Vega</div>
+            </div>
+
+            <div className="cast">
+              <div className="cast-name">
+                <b>John Travolta</b>
+              </div>
+              <div className="cast-as">as Vincent Vega</div>
+            </div>
+
+            <div className="cast">
+              <div className="cast-name">
+                <b>John Travolta</b>
+              </div>
+              <div className="cast-as">as Vincent Vega</div>
+            </div>
+          </div>
         </div>
-        <div className="cast-innersection"></div>
-        <p className="cast-name">John Travolta</p>
-        <p className="cast-as">as Vincent Vega</p>
       </div>
 
       <div className="movie-box">
-        <div className="rating">8,9%</div>
+        <div className="rating">
+          <b>8,9</b>
+          <small>/10</small>
+        </div>
         <img src="../assets/pulp-fiction.jpg" alt="pulp-fiction poster" className="poster" />
         <div className="info">
           <h2 className="title">Pulp Fiction</h2>
           <div className="meta">
             <p className="year">
-              <b>Rok vydání:</b> 1994
+              <b>Rok vydání:</b> <span>1994</span>
             </p>
             <p className="genre">
-              <b>Žárn:</b> crime, drama
+              <b>Žárn:</b> <span>crime, drama</span>
             </p>
             <p className="director">
-              <b>Režie:</b> Quentin Tarantino
+              <b>Režie:</b> <span>Quentin Tarantino</span>
             </p>
           </div>
-          <h3 className="cast-title">V hlavních rolích</h3>
+          <h3 className="cast-title">V hlavních rolích:</h3>
+          <div className="cast-innersection">
+            <div className="cast">
+              <div className="cast-name">
+                <b>John Travolta</b>
+              </div>
+              <div className="cast-as">
+                as <b>Vincent Vega</b>
+              </div>
+            </div>
+
+            <div className="cast">
+              <div className="cast-name">
+                <b>John Travolta</b>
+              </div>
+              <div className="cast-as">as Vincent Vega</div>
+            </div>
+
+            <div className="cast">
+              <div className="cast-name">
+                <b>John Travolta</b>
+              </div>
+              <div className="cast-as">as Vincent Vega</div>
+            </div>
+
+            <div className="cast">
+              <div className="cast-name">
+                <b>John Travolta</b>
+              </div>
+              <div className="cast-as">as Vincent Vega</div>
+            </div>
+          </div>
         </div>
-        <div className="cast-innersection"></div>
-        <p className="cast-name">John Travolta</p>
-        <p className="cast-as">as Vincent Vega</p>
+      </div>
+
+      <div className="movie-box">
+        <div className="rating">
+          <b>8,9</b>
+          <small>/10</small>
+        </div>
+        <img src="../assets/pulp-fiction.jpg" alt="pulp-fiction poster" className="poster" />
+        <div className="info">
+          <h2 className="title">Pulp Fiction</h2>
+          <div className="meta">
+            <p className="year">
+              <b>Rok vydání:</b> <span>1994</span>
+            </p>
+            <p className="genre">
+              <b>Žárn:</b> <span>crime, drama</span>
+            </p>
+            <p className="director">
+              <b>Režie:</b> <span>Quentin Tarantino</span>
+            </p>
+          </div>
+          <h3 className="cast-title">V hlavních rolích:</h3>
+          <div className="cast-innersection">
+            <div className="cast">
+              <div className="cast-name">
+                <b>John Travolta</b>
+              </div>
+              <div className="cast-as">
+                as <b>Vincent Vega</b>
+              </div>
+            </div>
+
+            <div className="cast">
+              <div className="cast-name">
+                <b>John Travolta</b>
+              </div>
+              <div className="cast-as">as Vincent Vega</div>
+            </div>
+
+            <div className="cast">
+              <div className="cast-name">
+                <b>John Travolta</b>
+              </div>
+              <div className="cast-as">as Vincent Vega</div>
+            </div>
+
+            <div className="cast">
+              <div className="cast-name">
+                <b>John Travolta</b>
+              </div>
+              <div className="cast-as">as Vincent Vega</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="movie-box">
+        <div className="rating">
+          <b>8,9</b>
+          <small>/10</small>
+        </div>
+        <img src="../assets/pulp-fiction.jpg" alt="pulp-fiction poster" className="poster" />
+        <div className="info">
+          <h2 className="title">Pulp Fiction</h2>
+          <div className="meta">
+            <p className="year">
+              <b>Rok vydání:</b> <span>1994</span>
+            </p>
+            <p className="genre">
+              <b>Žárn:</b> <span>crime, drama</span>
+            </p>
+            <p className="director">
+              <b>Režie:</b> <span>Quentin Tarantino</span>
+            </p>
+          </div>
+          <h3 className="cast-title">V hlavních rolích:</h3>
+          <div className="cast-innersection">
+            <div className="cast">
+              <div className="cast-name">
+                <b>John Travolta</b>
+              </div>
+              <div className="cast-as">
+                as <b>Vincent Vega</b>
+              </div>
+            </div>
+
+            <div className="cast">
+              <div className="cast-name">
+                <b>John Travolta</b>
+              </div>
+              <div className="cast-as">as Vincent Vega</div>
+            </div>
+
+            <div className="cast">
+              <div className="cast-name">
+                <b>John Travolta</b>
+              </div>
+              <div className="cast-as">as Vincent Vega</div>
+            </div>
+
+            <div className="cast">
+              <div className="cast-name">
+                <b>John Travolta</b>
+              </div>
+              <div className="cast-as">as Vincent Vega</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
